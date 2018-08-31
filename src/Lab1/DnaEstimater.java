c096d24ae8fd8d82226c4baa11acd51d220b1a4c
johnn <johnn@10.38.57.115>
2018-08-31 09:29:09 -0400

    1

--- src/Lab1/DnaEstimater.java
+++ src/Lab1/DnaEstimater.java
@@ -11,5 +11,10 @@ public class DnaEstimater {
 		
 		System.out.println(random.nextInt(4));
 		System.out.println(random.nextInt(4));
+		SampleSize = 1000;
+		for(i=1;i<SampleSize);++)
+		{
+			
+		}
 	}
 }
