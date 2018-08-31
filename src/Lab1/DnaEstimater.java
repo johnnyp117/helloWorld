3693d967fa677f784418790ca1e8f9222ad26c9d
johnn <johnn@10.38.57.115>
2018-08-31 09:27:55 -0400

    yup

---
+++ src/Lab1/DnaEstimater.java
@@ -0,0 +1,15 @@
+package Lab1;
+import java.util.Random;
+/**
+ * @author johnn
+ *
+ */
+public class DnaEstimater {
+	public static void main(String[] args)
+	{
+		Random random = new Random();
+		
+		System.out.println(random.nextInt(4));
+		System.out.println(random.nextInt(4));
+	}
+}
