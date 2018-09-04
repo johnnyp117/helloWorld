8d75fbf764e379afc06fbeee90dc3586b972dfdd
johnn <johnn@10.38.57.115>
2018-09-04 15:50:48 -0400

    update 9.4.18.1550

--- src/Lab1/DnaEstimater.java
+++ src/Lab1/DnaEstimater.java
@@ -1,5 +1,6 @@
 package Lab1;
 import java.util.Random;
+import java.lang.StringBuilder;
 /**
  * @author johnn
  *
@@ -7,14 +8,42 @@ import java.util.Random;
 public class DnaEstimater {
 	public static void main(String[] args)
 	{
+		//* Local variables
 		Random random = new Random();
-		
-		System.out.println(random.nextInt(4));
-		System.out.println(random.nextInt(4));
-		SampleSize = 1000;
-		for(i=1;i<SampleSize);++)
+		String Nucleotides[] = {"A","C","T","G"};
+		int count = 0;
+		int countA, countC, countT, countG  ;
+		countA = countC = countT = countG = 0;
+		StringBuilder Sequence = new StringBuilder();
+		//* Runtime
+		for(int i=1;i<3000;i=i+1)
 		{
-			
+			Sequence.append(Nucleotides[random.nextInt(4)]);
+			switch(Sequence.substring(i-1))
+			{
+			case "A": countA++;
+			break;
+			case "C":countC++;
+			break;
+			case "G":countG++;
+			break;
+			case "T":countT++;
+			break;
+			}
+			if(i%3 == 0)
+				{	
+					String Trimers =  Sequence.substring(i-3,i);
+					//System.out.println(Trimers);
+					if( Trimers.equals("AAA"))
+					{
+						count++;
+						//* p(AAA) = n choose k for all trimers assuming even dist.
+					}
+				}
 		}
+		System.out.printf("AAA trimer was found %s times %n",count);
+		System.out.printf("A %s, T %s, C %s, G %s %n",countA, countT, countC, countG);
+		double total = countA + countT + countC + countG;
+		System.out.printf("p(A) %f, p(T) %f, p(C) %f, p(G) %f",countA/total, countT/total, countC/total, countG/total);
 	}
 }
