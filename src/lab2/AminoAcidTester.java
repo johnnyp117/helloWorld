814dd756530ceaeef82ae65e2128e1e95f5ebdcf
johnn <johnn@192.168.0.4>
2018-09-17 20:32:51 -0400

    Added some extra credit stuff

--- src/lab2/AminoAcidTester.java
+++ src/lab2/AminoAcidTester.java
@@ -5,15 +5,18 @@ import java.util.Random;
  * @author johnn
  * Date : 16.9.18
  * 
- * Base level requirement for lab2 quiz class
+ * Some extra credit level requirement for lab2 quiz class
+ * EC: Pass length of quiz, in time mode will return worst residue at end
  */
 public class AminoAcidTester
 {
 	public static void main(String[] args) 
 	{	 
 // Variables for game
-	boolean timer = false;
+	boolean timer, gameEnd, timerFlag, surviveFlag; 
+	timer = gameEnd = timerFlag = surviveFlag = false;
 	int scoreVar, questVar;
+	String timerVar = null;
 	scoreVar = questVar = 0;
 	final String[] SHORT_NAMES = { "A","R", "N", "D", "C", "Q", "E", 
 			"G",  "H", "I", "L", "K", "M", "F", 
@@ -22,16 +25,25 @@ public class AminoAcidTester
 			"aspartic acid", "cysteine","glutamine",  "glutamic acid",
 			"glycine" ,"histidine","isoleucine","leucine",  "lysine", "methionine", 
 			"phenylalanine", "proline","serine","threonine","tryptophan","tyrosine", "valine"};
+	int[] errorArray = new int[SHORT_NAMES.length]; 
 // Opening of game
-	System.out.println("Do you want to play a game?");
-	String inString = System.console().readLine().toUpperCase();
-		if( inString.equals("N"))
+	System.out.println("You know why you're here. Do you want to play timed or survival?");
+	String inString = System.console().readLine();
+		if( inString.equals("timed"))
 		{
-			System.out.println("too bad, dance puppet.");
+			System.out.println("Enter seconds desired to play");
+			timerVar = System.console().readLine();
+			timerFlag = true;
+		}
+		else if ( inString.equals("survival"))
+		{
+			System.out.println("Get ready");
+			surviveFlag = true;
+			
 		}
 	long startTime = System.currentTimeMillis();
-// Start the actual game loop	
-	while(timer == false)
+// Start the actual game loop with flag controls for versions	
+	while(gameEnd == false)
 	{
 		questVar++;
 		int idx = new Random().nextInt(FULL_NAMES.length);
@@ -42,22 +54,40 @@ public class AminoAcidTester
 			System.out.println("Ok, "+ ansString +" was correct. Next...");
 			scoreVar++;
 			}
-			else
+			else if(surviveFlag == true)
 			{
 			System.out.println(ansString+" was wrong :p. You needed "+SHORT_NAMES[idx]);
-			break;
+			gameEnd = true;
 			}
-		timer = evalTime(startTime);
+			else
+			{
+			errorArray[idx]++;
+			}
+		if(timerFlag == true)
+		{
+		timer = evalTime(startTime, timerVar);
+		gameEnd = timer;
+		}
 	}
 // End actual game loop, prints out results
 	System.out.println("Game over");
-	System.out.println("Your score is "+ scoreVar +" out of possible "+ questVar);
-	}
-// Method to evaluate time
-	public static Boolean evalTime(long timeVar)
+	if(timerFlag == true)
 	{
-		long startTime = System.currentTimeMillis();
-		if((startTime-timeVar)>30000)
+	System.out.println(timerVar + " seconds is up knave!");
+	System.out.println("Your score is "+ scoreVar +" out of possible "+ questVar);
+	System.out.println("Your worst residue was "+ FULL_NAMES[getIndexOfLargest(errorArray)]);
+	}
+	else
+	{
+	System.out.println("Your score is "+ scoreVar);
+	}
+	}
+	
+// Method to evaluate time
+	public static Boolean evalTime(long startTimeVar, String timeLengthVar)
+	{
+		long currentTime = System.currentTimeMillis();
+		if((currentTime-startTimeVar)>(Integer.parseInt(timeLengthVar)*1000))
 		{
 			return true;
 		}
@@ -65,6 +95,17 @@ public class AminoAcidTester
 		{
 			return false;
 		}
+	}
+	public static int getIndexOfLargest( int[] array )
+	{
+	  if ( array == null || array.length == 0 ) return -1; // null or empty
+
+	  int largest = 0;
+	  for ( int i = 1; i < array.length; i++ )
+	  {
+	      if ( array[i] > array[largest] ) largest = i;
+	  }
+	  return largest; // position of the first largest found
 	}
 
 }
