dae6ae219315986e3267460f899334455987aa6c
johnn <johnn@192.168.0.4>
2018-09-24 20:38:14 -0400

    .

--- src/lab3/ReadFASTA.java
+++ src/lab3/ReadFASTA.java
@@ -3,6 +3,8 @@ package lab3;
 public class ReadFASTA {
 	public static void main(String[] args) 
 	{
+		System.out.println("You know why you're here. Do you want to play (t)imed or (s)urvival?");
+		String inString = System.console().readLine();
 		
 	}
 }
