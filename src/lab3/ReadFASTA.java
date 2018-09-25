a898b707493b2bf9776f7aa41b5786c1f4cdbe15
johnn <johnn@192.168.0.4>
2018-09-24 23:31:54 -0400

    prelim

--- src/lab3/ReadFASTA.java
+++ src/lab3/ReadFASTA.java
@@ -1,10 +1,44 @@
 package lab3;
 
-public class ReadFASTA {
-	public static void main(String[] args) 
+import java.io.*;
+import java.util.*;
+/**
+ * @author John Patterson 
+ * Date : 24.9.18
+ * 
+ * Read in FASTA file, spit out tsv file.
+ */
+public class ReadFASTA 
+{
+	public String[] sequenceID  = new String[0];
+	public String[] sequence  = new String[0];
+	int numA = 0;
+	int numC = 0;
+	int numT = 0;
+	int numG = 0;
+	public static void main(String[] args) throws Exception
 	{
-		System.out.println("You know why you're here. Do you want to play (t)imed or (s)urvival?");
+		System.out.println("Please enter the name of the FASTA file.");
 		String inString = System.console().readLine();
+		String fullFilePath = System.getProperty("user.dir") +inString;
+		BufferedReader FASTAfile = new BufferedReader(new FileReader(new File(fullFilePath)));
 		
+		int lineCount = 0;
+		for(String nextLine = FASTAfile.readLine(); nextLine != null; nextLine = FASTAfile.readLine())
+		{
+			if( nextLine.split(">")[1] == ">")
+			{
+				
+			}
+			else 
+			{
+				continue;
+			}
+			
+			
+		}
+		FASTAfile.close();	
 	}
+	
+	
 }
