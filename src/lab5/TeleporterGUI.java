3b8d6c693fe8d4ae200b612349925551693b8185
johnn <johnn@10.216.3.208>
2018-10-24 21:00:34 -0400

    Basic assignment criteria. Can save text file, will be able to load from a text file. Certain inputs will have response, otherwise simply saves input text and plays gif.

--- src/lab5/TeleporterGUI.java
+++ src/lab5/TeleporterGUI.java
@@ -1,25 +1,28 @@
 package lab5;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 /**
  * @author John Patterson 
  * Date : 12.10.18
  * 
  * GUI to demo basic swing functionality
  * 
- * Class methods 
  */
-
-
 public class TeleporterGUI extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	private JPanel panel = new JPanel(); // the panel is not visible in output
 	private JTextField tf = new JTextField(10); // accepts up to 10 characters
     private JTextArea ta = new JTextArea(); // Text Area at the Center
-    
-	public TeleporterGUI(String stationName) 
+    private JLabel imageLabel = new JLabel();
+
+	public TeleporterGUI(String stationName) throws Exception
 	{
 		super(stationName);
 		System.out.println("beeb boot doop");
@@ -32,11 +35,16 @@ public class TeleporterGUI extends JFrame
         mb.add(m1);
         mb.add(m2);
         JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m22 = new JMenuItem("Save as");
+        JMenuItem m12 = new JMenuItem("Save as");
+        m12.addActionListener(new saveToFile());
+        m11.setFont(new Font("Microgramma", Font.BOLD,25));
+        m12.setFont(new Font("Microgramma", Font.BOLD,25));
+        JMenuItem m21 = new JMenuItem("No Help For You");
         m1.add(m11);
-        m1.add(m22);
+        m1.add(m12);
+        m2.add(m21);
 //Button set up
-    	JLabel label = new JLabel("Enter Text");
+    	JLabel label = new JLabel("Enter Command");
         JButton send = new JButton("Send");
         JButton reset = new JButton("Reset");
         label.setFont(new Font("Microgramma", Font.BOLD,25));
@@ -49,14 +57,27 @@ public class TeleporterGUI extends JFrame
         panel.add(tf);
         panel.add(send);
         panel.add(reset);
+        panel.setBorder(new LineBorder(Color.BLUE, 4));
+     // add the image label
+        String current = new java.io.File( "." ).getCanonicalPath() +"\\src\\lab5\\teleporting.gif";
+        ImageIcon ii = new ImageIcon(current);
+        imageLabel.setIcon(ii);
+        JLabel picur = new JLabel(ii);
 //Adding Components to the frame.
+        getContentPane().add(BorderLayout.WEST, picur);
         getContentPane().add(BorderLayout.SOUTH, panel);
         getContentPane().add(BorderLayout.NORTH, mb);
         getContentPane().add(BorderLayout.CENTER, ta);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1200, 1200);
         setVisible(true);
-//Setting overlay for pretty
+//Settings for pretty
+        panel.setBackground(Color.BLACK);
+        ta.setBackground(Color.BLACK);
+        setBackground(Color.BLACK);
+        ta.setForeground(Color.green);
+        label.setForeground(Color.green);  
+        panel.setForeground(Color.green);
         try
     	{
     		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
@@ -66,10 +87,6 @@ public class TeleporterGUI extends JFrame
     		System.out.println("Could not set look and feel");
     	}
 	}
-    public static void main(String args[]) throws Exception
-    {
-        new TeleporterGUI("station1");
-    }
 	private class StringActionListener implements ActionListener
     {
         @Override
@@ -78,6 +95,13 @@ public class TeleporterGUI extends JFrame
 			updateTextArea(tf.getText()); 
 		}
     }
+	public void updateTextArea(String input)
+	{
+    	input = input + "\n";
+    	ta.setText(ta.getText() + input); 
+    	tf.setText("");
+		validate();
+	}
 	private class ResetActionListener implements ActionListener
     {
         @Override
@@ -86,13 +110,44 @@ public class TeleporterGUI extends JFrame
         	ta.setText(""); 
 		}
     }
-    public void updateTextArea(String input)
-	{
-    	input = input + "\n";
-    	ta.setText(ta.getText() + input); 
-    	tf.setText("");
-		validate();
-	}
+    private class saveToFile implements ActionListener
+    {
+    	@Override
+    	public void actionPerformed(ActionEvent arg0)
+    	{
+    		saveToFile();
+    	}
+    }
+    public void saveToFile()
+    {
+    	JFileChooser jfc = new JFileChooser();
+    	if(jfc.showSaveDialog(this) != JFileChooser.APPROVE_OPTION)
+    		return;
+    	if(jfc.getSelectedFile() == null)
+    		return;
+    	File chosenFile = jfc.getSelectedFile();
+    	if(jfc.getSelectedFile().exists())
+    	{
+    		String message = "File" + jfc.getSelectedFile().getName() + " exists. Overwrite?";
+    		if(JOptionPane.showConfirmDialog(this, message) != JOptionPane.YES_OPTION)
+				return;
+    	}
+    	try
+    	{
+    		BufferedWriter writer = new BufferedWriter(new FileWriter(chosenFile));
+    		writer.write(ta.getText());
+    		writer.close();
+    	}
+    	catch(Exception ex)
+    	{
+    		ex.printStackTrace();
+    		JOptionPane.showMessageDialog(this, ex.getMessage(), "Could not write file", JOptionPane.ERROR_MESSAGE);
+    	}
+    }
+    public static void main(String args[]) throws Exception
+    {
+        new TeleporterGUI("station1");
+    }
 	
 
 }
